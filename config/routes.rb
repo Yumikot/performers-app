090a5d916792edc4e0e88404ac8c9a75a634957a
Yumiko Tezuka <ytezuka5@gmail.com>
2020-10-07 18:53:07 +0900

    postsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show, :edit, :update] 
-
+  root "posts#index"
+  resources :posts
 end
