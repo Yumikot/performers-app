4724808020d3517e552c437a4204a149616192aa
Yumiko Tezuka <ytezuka5@gmail.com>
2020-10-11 22:22:03 +0900

    カテゴリーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/edit'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show, :edit, :update] 
   root "posts#index"
   resources :posts
+  resources :categories, except: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_065423) do
+ActiveRecord::Schema.define(version: 2020_10_11_094604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_10_11_065423) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "image"
+    t.integer "category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
