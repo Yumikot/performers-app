144e7623434b883658d11182a7be82a818f91845
Yumiko Tezuka <ytezuka5@gmail.com>
2020-10-16 22:54:08 +0900

    scssを反映させる修正中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-//= require jquery3
-//= require rails-ujs
 //= require_tree .
 require("bootstrap/dist/js/bootstrap")
