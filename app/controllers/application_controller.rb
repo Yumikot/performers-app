e0166f5b0789d68608566704bb9f90f03eec19dc
Yumiko Tezuka <ytezuka5@gmail.com>
2021-02-01 22:46:28 +0900

    end消し忘れ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+ 
   protect_from_forgery with: :exception
 
  
@@ -15,5 +16,4 @@ class ApplicationController < ActionController::Base
   end
   
   
-  end
 end
